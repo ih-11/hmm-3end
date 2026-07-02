7e87755b99a4d773ea39e5522a91516829b5a49d
ih-11 <ibnubioeng@gmail.com>
2026-07-02 12:11:50 +0900

    up

--- scripts/miningA.py
+++ scripts/miningA.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+import argparse
+import gzip
+import re
+
+parse = argparse.ArgumentParser()
+parse.add_argument("fastq", help = "Input FASTQ.gz")
+parse.add_argument("--min-a", type=int, default=15, help="Min Poly-A length")
+parse.add_argument("--up", type=int, default=100, help="Upstream Seq. of Poly-A")
+arg = parse.parse_args()

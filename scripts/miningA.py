d9f62418e2ce6c187c262fd6395090637fdb2aa4
ih-11 <ibnubioeng@gmail.com>
2026-07-03 15:04:58 +0900

    up

--- scripts/miningA.py
+++ scripts/miningA.py
@@ -7,14 +7,13 @@ import re
 parser = argparse.ArgumentParser()
 parser.add_argument("fastq", help="Input FASTQ.gz")
 parser.add_argument("--min-len", type=int, default=15, help="Minimum A/T run length")
-parser.add_argument("--up", type=int, default=100, help="Upstream sequence length")
-parser.add_argument("--down", type=int, default=20, help="Downstream sequence lenght")
-parser.add_argument("--tail", choices=["A", "T", "both"], default="A", help="Tail type A/T")
+parser.add_argument("--up", type=int, default=100, help="Long side length")
+parser.add_argument("--down", type=int, default=20, help="Short side length")
+parser.add_argument("--tail", choices=["A", "T"], required=True, help="Tail type A/T")
 
 args = parser.parse_args()
 
-
-pattern = re.compile(args.tail * args.min_a + "+")
+pattern = re.compile(args.tail * args.min_len + "+")
 
 with gzip.open(args.fastq, "rt") as f:
     while True:
@@ -33,9 +32,10 @@ with gzip.open(args.fastq, "rt") as f:
             if args.tail == "A":
                 start = match.start() - args.up
                 end = match.end() + args.down
-            else:   
+            else:
                 start = match.start() - args.down
                 end = match.end() + args.up
+
             if start < 0:
                 start = 0
             if end > len(seq):

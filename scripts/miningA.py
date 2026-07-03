2b407f61f3da59fa36c48c3d66ddb15a9b51117e
ih-11 <ibnubioeng@gmail.com>
2026-07-03 14:59:48 +0900

    up

--- scripts/miningA.py
+++ scripts/miningA.py
@@ -6,11 +6,13 @@ import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("fastq", help="Input FASTQ.gz")
-parser.add_argument("--min-a", type=int, default=15, help="Minimum A/T run length")
+parser.add_argument("--min-len", type=int, default=15, help="Minimum A/T run length")
 parser.add_argument("--up", type=int, default=100, help="Upstream sequence length")
-parser.add_argument("--tail", choices=["A", "T", "both"], default="A",
-                    help="Tail type to search for")
+parser.add_argument("--down", type=int, default=20, help="Downstream sequence lenght")
+parser.add_argument("--tail", choices=["A", "T", "both"], default="A", help="Tail type A/T/N")
+
 args = parser.parse_args()
+
 
 if args.tail == "A":
     patterns = [("A", re.compile("A" * args.min_a + "+"))]
@@ -19,10 +21,12 @@ elif args.tail == "T":
 else:
     patterns = [
         ("A", re.compile("A" * args.min_a + "+")),
-        ("T", re.compile("T" * args.min_a + "+")),
+        ("T", re.compile("T" * args.min_a + "+"))
     ]
 
+
 with gzip.open(args.fastq, "rt") as f:
+
     while True:
         header = f.readline().strip()
         seq = f.readline().strip()
@@ -38,12 +42,19 @@ with gzip.open(args.fastq, "rt") as f:
             match = pattern.search(seq)
 
             if match:
-                start = match.start() - args.up
+                if tail_type == "A":
+                    start = match.start() - args.up
+                    end = match.end() + args.down
+                else:
+                    start = match.start() - args.down
+                    end = match.end() + args.up
                 if start < 0:
                     start = 0
-
-                outseq = seq[start:]
+                if end > len(seq):
+                    end = len(seq)
+                outseq = seq[start:end]
 
                 print(f">{read_id}_{tail_type}")
                 print(outseq)
+
                 break

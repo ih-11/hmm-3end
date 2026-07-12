27d3094e4b746cba5d599a41934fb6c2c13f798d
ih-11 <ibnubioeng@gmail.com>
2026-07-12 22:03:11 +0900

    up

--- scripts/miningA.py
+++ scripts/miningA.py
@@ -2,36 +2,24 @@
 
 import argparse
 import re
-import sys
 import korflab
 
 parser = argparse.ArgumentParser(
-	description='extract fixed-length 3end candidate sequences')
+	description='extract fixed-length sequence around poly-A or poly-T runs')
 parser.add_argument('fastq', help='input fastq file')
 parser.add_argument('--min-len', type=int, default=15,
 	help='minimum A/T run length')
 parser.add_argument('--seq-len', type=int, default=100,
-	help='sequence length before tail')
+	help='transcript-side sequence length')
 parser.add_argument('--tail-len', type=int, default=20,
 	help='tail length in output')
-parser.add_argument('--report', type=int, default=0,
-	help='report progress every N reads')
 arg = parser.parse_args()
 
 apat = re.compile('A' * arg.min_len + '+')
 tpat = re.compile('T' * arg.min_len + '+')
 
-total = 0
-out = 0
-ahit = 0
-thit = 0
-edge = 0
-
-for name, seq, plus, qual in korflab.readfastq(arg.fastq):
-	total += 1
-
-	if arg.report > 0 and total % arg.report == 0:
-		print(f'[progress] processed {total} reads', file=sys.stderr)
+for header, seq, plus, qual in korflab.readfastq(arg.fastq):
+	read_id = header.split()[0]
 
 	am = apat.search(seq)
 	tm = tpat.search(seq)
@@ -40,37 +28,20 @@ for name, seq, plus, qual in korflab.readfastq(arg.fastq):
 		beg = am.start() - arg.seq_len
 		end = am.start() + arg.tail_len
 
-		if beg < 0 or end > len(seq):
-			edge += 1
-			continue
+		if beg < 0 or end > len(seq): continue
 
 		outseq = seq[beg:end]
-		run = len(am.group())
-		ahit += 1
 
 	elif tm:
 		beg = tm.end() - arg.tail_len
 		end = tm.end() + arg.seq_len
 
-		if beg < 0 or end > len(seq):
-			edge += 1
-			continue
+		if beg < 0 or end > len(seq): continue
 
 		outseq = korflab.anti(seq[beg:end])
-		run = len(tm.group())
-		thit += 1
 
 	else:
 		continue
 
-	read_id = name.split()[0]
-	print(f'>{read_id} run={run} len={len(outseq)}')
+	print(f'>{read_id}')
 	print(outseq)
-	out += 1
-
-print('[summary]', file=sys.stderr)
-print('input_reads', total, sep='\t', file=sys.stderr)
-print('output_reads', out, sep='\t', file=sys.stderr)
-print('polyA_reads', ahit, sep='\t', file=sys.stderr)
-print('polyT_reads', thit, sep='\t', file=sys.stderr)
-print('edge_skipped', edge, sep='\t', file=sys.stderr)

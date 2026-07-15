a9657e816ff2430bb4053fce3caf0a8b88bca9de
ih-11 <ibnubioeng@gmail.com>
2026-07-16 00:07:41 +0900

    up

--- scripts/miningA.py
+++ scripts/miningA.py
@@ -2,7 +2,6 @@
 
 import argparse
 import re
-import sys
 import korflab
 
 parser = argparse.ArgumentParser(
@@ -12,23 +11,12 @@ parser.add_argument('--min-len', type=int, default=15,
 	help='minimum A/T run length')
 parser.add_argument('--seq-len', type=int, default=100,
 	help='transcript-side sequence length')
-parser.add_argument('--report', type=int, default=0,
-	help='report progress every N reads')
 arg = parser.parse_args()
 
 apat = re.compile('A' * arg.min_len + '+')
 tpat = re.compile('T' * arg.min_len + '+')
 
-total = 0
-output = 0
-skipped = 0
-
 for header, seq, plus, qual in korflab.readfastq(arg.fastq):
-	total += 1
-
-	if arg.report > 0 and total % arg.report == 0:
-		print(f'processed {total} reads', file=sys.stderr)
-
 	read_id = header.split()[0]
 
 	am = apat.search(seq)
@@ -37,9 +25,7 @@ for header, seq, plus, qual in korflab.readfastq(arg.fastq):
 	if am:
 		beg = am.start() - arg.seq_len
 
-		if beg < 0:
-			skipped += 1
-			continue
+		if beg < 0: continue
 
 		outseq = seq[beg:]
 		run = len(am.group())
@@ -47,9 +33,7 @@ for header, seq, plus, qual in korflab.readfastq(arg.fastq):
 	elif tm:
 		end = tm.end() + arg.seq_len
 
-		if end > len(seq):
-			skipped += 1
-			continue
+		if end > len(seq): continue
 
 		outseq = korflab.anti(seq[:end])
 		run = len(tm.group())
@@ -59,9 +43,3 @@ for header, seq, plus, qual in korflab.readfastq(arg.fastq):
 
 	print(f'>{read_id} lenA={run} len_read={len(seq)}')
 	print(outseq)
-	output += 1
-
-print('\nsummary', file=sys.stderr)
-print('input_reads', total, sep='\t', file=sys.stderr)
-print('output_reads', output, sep='\t', file=sys.stderr)
-print('skipped_reads', skipped, sep='\t', file=sys.stderr)

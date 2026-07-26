e41a290090d5a09a490616902430a3d20d0f93a3
ih-11 <ibnubioeng@gmail.com>
2026-07-26 22:33:08 +0900

    up

--- scripts/preliminaryA.py
+++ scripts/preliminaryA.py
@@ -4,16 +4,10 @@ import argparse
 import re
 import korflab
 
-parser = argparse.ArgumentParser(
-	description='extract candidate 3end sequences')
+parser = argparse.ArgumentParser(description='extract candidate 3end sequences')
 parser.add_argument('fastq', help='input fastq file')
-parser.add_argument('--min-len', type=int, default=15,
-	help='minimum A/T run length')
-parser.add_argument('--seq-len', type=int, default=100,
-	help='transcript-side sequence length')
-parser.add_argument('--adapter',
-	default='CTTGCGGGCGGCGGACTCTC',
-	help='expected sequence downstream of poly-A')
+parser.add_argument('--min-len', type=int, default=15, help='minimum A/T run length')
+parser.add_argument('--seq-len', type=int, default=100, help='transcript-side sequence length')
 arg = parser.parse_args()
 
 apat = re.compile('A' * arg.min_len + '+')
@@ -25,31 +19,32 @@ for header, seq, plus, qual in korflab.readfastq(arg.fastq):
 	am = apat.search(seq)
 	tm = tpat.search(seq)
 
-	if am:
-		beg = am.start() - arg.seq_len
+	# The variable 'keep' is called a FLAG
+	keep = None
+	if am and tm: keep = False
+	elif am:      keep = True
+	elif tm:      keep = True
+	else:         keep = False
 
-		if beg < 0:
-			continue
+	if keep:
+		if am:
+			beg = am.start() - arg.seq_len
 
-		outseq = seq[beg:]
-		run = len(am.group())
+			if beg < 0:
+				keep = False
+			else:
+				outseq = seq[beg:am.end()]
+				run = len(am.group())
 
-	elif tm:
-		end = tm.end() + arg.seq_len
+		elif tm:
+			end = tm.end() + arg.seq_len
 
-		if end > len(seq):
-			continue
+			if end > len(seq):
+				keep = False
+			else:
+				outseq = korflab.anti(seq[tm.start():end])
+				run = len(tm.group())
 
-		outseq = korflab.anti(seq[:end])
-		run = len(tm.group())
-
-	else:
-		continue
-
-	downstream = outseq[arg.seq_len + run:]
-
-	if not downstream.startswith(arg.adapter):
-		continue
-
-	print(f'>{read_id} lenA={run} len_read={len(seq)}')
-	print(outseq)
+	if keep:
+		print(f'>{read_id} lenA={run} len_read={len(seq)}')
+		print(outseq)
